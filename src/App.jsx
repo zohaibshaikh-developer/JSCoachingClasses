143b2c6026ab2a428aa85db2c6eff9a8b94a79f2
zohaibshaikh-developer <zohaibshaikhdeveloper@gmail.com>
2023-07-10 21:11:50 +0530

    Code Pushed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';// Import your custom styles first
+import 'tailwindcss/tailwind.css'; // Import Tailwind CSS last
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
